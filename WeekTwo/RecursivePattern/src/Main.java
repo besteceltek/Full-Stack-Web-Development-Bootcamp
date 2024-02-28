fdd5725b282b74252b42d2159cd52c456aa70fac
Beste Çeltek <besteceltek@gmail.com>
2024-02-28 18:47:59 +0300

    Added comments

--- WeekTwo/RecursivePattern/src/Main.java
+++ WeekTwo/RecursivePattern/src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Defined the variables
         int count;
 
+        //Called the Scanner class and prompted the number from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter a number: ");
         count = input.nextInt();
@@ -11,11 +13,13 @@ public class Main {
         Pattern(count);
     }
 
+    //Created the method for printing the pattern
     static void Pattern(int number) {
         if (number <= 0) {
             System.out.print(number + " ");
             return;
         }
+        //Used mirroring technique for printing the numbers after negative value
         System.out.print(number + " ");
         Pattern(number - 5);
         System.out.print(number + " ");
