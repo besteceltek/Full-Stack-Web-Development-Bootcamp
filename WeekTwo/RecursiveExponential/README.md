6c49ad9dda24368ea4cd67fb3c0fd75faeadcc54
Beste Çeltek <besteceltek@gmail.com>
2024-02-28 19:06:01 +0300

    Create README.md

---
+++ WeekTwo/RecursiveExponential/README.md
@@ -0,0 +1,5 @@
+# Exponential Numbers with Recursive Method
+
+A program that takes the base and exponent values from 
+the user and performs exponentiation using the 
+recursive method.
--- WeekTwo/RecursiveExponential/src/Main.java
+++ WeekTwo/RecursiveExponential/src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Defined variables
         int base, exponent;
 
+        //Called the Scanner class and prompted the base and exponent numbers from the user
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter the base number: ");
         base = input.nextInt();
@@ -13,11 +15,13 @@ public class Main {
         System.out.println(base + " to the power of " + exponent + " is " + Exponential(base, exponent));
     }
 
+    //Created a method to calculate the exponential numbers
     static int Exponential(int base, int exponent) {
         if (exponent == 0){
             return 1;
         }
         else {
+            //Used recursive method to multiply the base number as many as exponential value
             int result = base * Exponential(base,exponent - 1);
             return result;
         }
