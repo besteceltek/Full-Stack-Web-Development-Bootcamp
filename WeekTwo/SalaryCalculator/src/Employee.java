c75153fa5a3e9f1b4df2341a2e2538bd4f913071
Beste Çeltek <besteceltek@gmail.com>
2024-03-02 15:24:31 +0300

    Initial commit

--- WeekTwo/SalaryCalculator/src/Employee.java
+++ WeekTwo/SalaryCalculator/src/Employee.java
@@ -1,13 +1,18 @@
 public class Employee {
     String name;
-    int salary, workHours, hireYear;
-    double tax;
+    int workHours, hireYear, bonus;
+    double salary, tax, raise, taxSalary, totalSalary;
 
     Employee(String name,int salary,int workHours,int hireYear) {
         this.name = name;
         this.salary = salary;
         this.workHours = workHours;
         this.hireYear = hireYear;
+        tax = tax();
+        bonus = bonus();
+        raise = raiseSalary();
+        taxSalary = this.salary - tax + bonus;
+        totalSalary = taxSalary + raise;
     }
     double tax() {
         if (this.salary < 1000) {
@@ -19,18 +24,35 @@ public class Employee {
         return tax;
     }
 
-    void bonus() {
-
+    int bonus() {
+        if (this.workHours > 40) {
+            bonus = (this.workHours - 40) * 30;
+        } else {
+            bonus = 0;
+        }
+        return bonus;
     }
 
-    void raiseSalary() {
-
+    double raiseSalary() {
+        double raise;
+        if ((2024 - this.hireYear) < 10) {
+            raise = this.salary * 0.05;
+        } else if ((2024 - this.hireYear) > 9 && (2024 - this.hireYear) < 20) {
+            raise = this.salary * 0.10;
+        } else {
+            raise = this.salary * 0.15;
+        }
+        return raise;
     }
     void printEmp() {
-        System.out.println(this.name);
-        System.out.println(this.salary);
-        System.out.println(this.workHours);
-        System.out.println(this.hireYear);
-        System.out.println("Tax: " + tax());
+        System.out.println("Name: " + this.name);
+        System.out.println("Salary: " + this.salary);
+        System.out.println("Work Hours: " + this.workHours);
+        System.out.println("Hire Year: " + this.hireYear);
+        System.out.println("Tax: " + tax);
+        System.out.println("Bonus: " + bonus);
+        System.out.println("Raise: " + raise);
+        System.out.println("Salary with the Tax and Bonus: " + taxSalary);
+        System.out.println("Total Salary: " + totalSalary);
     }
 }
--- WeekTwo/SalaryCalculator/src/Main.java
+++ WeekTwo/SalaryCalculator/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         Employee kemal = new Employee("kemal", 2000, 45, 1985);
         kemal.printEmp();
     }
