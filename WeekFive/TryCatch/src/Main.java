719344ee7ce21a5a0d79a26574a5a5ac4ec985d1
Beste Çeltek <besteceltek@gmail.com>
2024-03-24 23:05:49 +0300

    Added comments

--- WeekFive/TryCatch/src/Main.java
+++ WeekFive/TryCatch/src/Main.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Create a 10 element array
         int[] array = {0,1,2,3,4,5,6,7,8,9};
+
+        // Create a Scanner object
         Scanner input = new Scanner(System.in);
+
+        // Index prompt from user
         System.out.print("Please enter a number: ");
         int index = input.nextInt();
 
+        // try-catch block to handle the error
         try {
             System.out.println("Element at index " + index + " is " + arrayElementAtIndex(array, index));
         } catch (ArrayIndexOutOfBoundsException e) {
@@ -14,6 +20,7 @@ public class Main {
         }
     }
 
+    // Method returns array element in an index prompted
     public static int arrayElementAtIndex(int[] array, int index) {
         return array[index];
     }
