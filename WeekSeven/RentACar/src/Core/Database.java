a261247ae585a0c2046277b44d928b7d62237935
Beste Çeltek <besteceltek@gmail.com>
2024-05-03 16:51:08 +0300

    Updated database

--- WeekSeven/RentACar/src/Core/Database.java
+++ WeekSeven/RentACar/src/Core/Database.java
@@ -1,23 +1,35 @@
 package Core;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Database {
     // Singleton Design Pattern
     private static Database instance = null;
     private Connection connection = null;
-    private final String DB_URL = "jdbc:postgresql://localhost:5432/rentacar";
-    private final String DB_USERNAME = "postgres";
-    private final String DB_PASSWORD = "1234";
 
     private Database() {
         // Database Connection
         try {
+            Properties properties = new Properties();
+            FileInputStream input = new FileInputStream("/Users/besteceltek/Desktop/Full-Stack-Web-Development-Bootcamp/WeekSeven/RentACar/database.properties");
+            properties.load(input);
+            input.close();
+
+            String DB_URL = properties.getProperty("DB_URL");
+            String DB_USERNAME = properties.getProperty("DB_USERNAME");
+            String DB_PASSWORD = properties.getProperty("DB_PASSWORD");
+
             this.connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
