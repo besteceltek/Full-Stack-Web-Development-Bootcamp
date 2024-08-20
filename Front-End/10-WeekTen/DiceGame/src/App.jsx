09790b7175e1f1efad45f7420fb6093dbca8941f
Beste Çeltek <besteceltek@gmail.com>
2024-08-21 00:57:46 +0300

    Added dice roll

--- Front-End/10-WeekTen/DiceGame/src/App.jsx
+++ Front-End/10-WeekTen/DiceGame/src/App.jsx
@@ -1,13 +1,48 @@
-import { Component } from "react";
-import { useState } from 'react'
+import React, { useState } from "react";
 import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShuffle } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const [player1Roll, setPlayer1Roll] = useState(1);
+  const [player2Roll, setPlayer2Roll] = useState(1);
+
+  const rollDice = () => {
+    const rollInterval = setInterval(() => {
+      setPlayer1Roll(Math.floor(Math.random() * 6) + 1);
+      setPlayer2Roll(Math.floor(Math.random() * 6) + 1);
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(rollInterval)
+    },3000)
+  };
 
   return (
     <>
-      
+      <div className='header'>
+        <h1>Draw! 🤝🏻</h1>
+      </div>
+      <div className='game'>
+        <div className="player">
+          <h2 className='player-name'>Player 1</h2>
+          <Dice face={player1Roll} />
+        </div>
+        <div className="player">
+          <h2 className='player-name'>Player 2</h2>
+          <Dice face={player2Roll} />
+        </div>
+      </div>
+      <div className='play-button'>
+        <button onClick={rollDice}><FontAwesomeIcon icon={faShuffle} /></button>
+      </div>
     </>
+  )
+}
+
+function Dice({ face }) {
+  return (
+    <img className="player-img" src={`/images/dice${face}.png`} alt={`Dice face ${face}`}/>
   )
 }
 
