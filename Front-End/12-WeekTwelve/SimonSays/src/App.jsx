c45749f9a7c225ba59bde0b497844de67da90b95
Beste Çeltek <besteceltek@gmail.com>
2024-09-03 21:04:04 +0300

    Added useState

--- Front-End/12-WeekTwelve/SimonSays/src/App.jsx
+++ Front-End/12-WeekTwelve/SimonSays/src/App.jsx
@@ -16,20 +16,27 @@ function App() {
   const [compArr, setCompArr] = useState([])
   const [userArr, setUserArr] = useState([])
   const [turn, setTurn] = useState(false)
+  const [score, setScore] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
 
-  useEffect(() => {
+  const handleStart = () => {
+    setIsPlaying(true)
+    computerTurn()
+  }
+
+  function computerTurn() {
     const randomNumber = Math.floor(Math.random() * 4) + 1
     setCompArr((prev) => [...prev, randomNumber]) 
-  }, [])
+  }
 
   useEffect(() => {
     const playCompArr = async () => {
       for (let i = 0; i < compArr.length; i++) {
         const pad = document.getElementById(compArr[i])
+        await sleep()
         pad.classList.add("active")
         await sleep()
         pad.classList.remove("active")
-        await sleep()
       }
       if (compArr.length !== 0) {
         setTurn((prev) => !prev)
@@ -38,20 +45,58 @@ function App() {
     playCompArr()
   }, [compArr])
 
+  useEffect(() => {
+    if (userArr.length === 0) return
+    if (userArr.length === compArr.length) {
+      if (JSON.stringify(userArr) === JSON.stringify(compArr)) {
+        setScore(prev => prev + 1)
+        setUserArr([])
+        setTimeout(() => {
+          computerTurn()
+        }, 1000)
+        setTurn((prev) => !prev)
+      }else {
+        setUserArr([])
+        setCompArr([])
+        setScore(0)
+        setIsPlaying(false)
+      }
+    }
+  }, [userArr])
+
+  const handleClick = async (e) => {
+    const id = parseInt(e.target.id)
+    setUserArr((prev) => [...prev, id])
+    const pad = document.getElementById(id)
+    pad.classList.add("active")
+    await sleep()
+    pad.classList.remove("active")
+  }
+
   return (
     <div className='App'>
-      <h3>{turn ? "Sıra sende" : "Sıra Simon'da"}</h3>
-      <div className='board'>
-        <div className='pads'>
-          {boardItem.map((item) => (
-            <div
-              key={item.name} 
-              id={item.id} 
-              className={`pad ${item.name}`}
-            ></div>
-          ))}
+      {isPlaying && (<h3>{turn ? "Sıra sende" : "Sıra Simon'da"}</h3>)}
+      <h3>Score: {score}</h3>
+      {!isPlaying && (
+        <div>
+          <h3>Simon Game</h3>
+          <button onClick={handleStart}>Start Game</button>
         </div>
-      </div>
+      )}
+      {isPlaying && (
+        <div className='board'>
+          <div className='pads'>
+            {boardItem.map((item) => (
+              <div
+                key={item.name} 
+                id={item.id} 
+                className={`pad ${item.name}`}
+                onClick={handleClick}
+              ></div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
